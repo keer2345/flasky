240e4263528f26ff0db53e2096da1363854a03c3
keer2345 <keer2345@gmail.com>
2017-03-14 23:22:49 +0800

    2c

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask.ext.script import Manager
+
 app = Flask(__name__)
+manager = Manager(app)
 
 
 @app.route('/')
@@ -13,4 +16,4 @@ def user(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
