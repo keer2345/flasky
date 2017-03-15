af65ccffca9ed080ed82d897ac49c686df8fdf24
keer2345 <keer2345@gmail.com>
2017-03-15 23:23:41 +0800

    base.html & 404.html

--- hello.py
+++ hello.py
@@ -17,7 +17,13 @@ def index():
 def user(name):
     # return '<h1>Hello, %s!!!</h1>' % name
     return render_template('user.html', name=name)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
 
 if __name__ == '__main__':
     manager.run()
