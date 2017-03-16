386c45cfd9f5c97234d998147860198e61c93fa4
keer2345 <keer2345@gmail.com>
2017-03-16 13:28:00 +0800

    moment

--- hello.py
+++ hello.py
@@ -1,29 +1,36 @@
+from datetime import datetime
 from flask import Flask, render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
     # return '<h1>Hello world!!!</h1>'
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
     # return '<h1>Hello, %s!!!</h1>' % name
     return render_template('user.html', name=name)
+
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'),404
+    return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'),500
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     manager.run()
