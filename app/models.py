8c6f85bc64ea1a7fc35fdcea314e35dc9e5c2737
keer2345 <keer2345@gmail.com>
2017-07-04 22:10:41 +0800

    list follower's posts 2

--- app/models.py
+++ app/models.py
@@ -206,9 +206,7 @@ class User(UserMixin, db.Model):
         return self.followers.filter_by(
             follower_id=user.id).first() is not None
 
-    @@property
-    def (self):
-        return 
+    @property
     def followed_posts(self):
         return Post.query.join(
             Follow, Follow.followed_id == Post.author_id).filter(
