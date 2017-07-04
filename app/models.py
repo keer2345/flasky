981cde367e6acb3dd7db0ab3c2bf4a99b1ae2b83
keer2345 <keer2345@gmail.com>
2017-07-04 22:34:07 +0800

    following self

--- app/models.py
+++ app/models.py
@@ -99,6 +99,7 @@ class User(UserMixin, db.Model):
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = hashlib.md5(
                 self.email.encode('utf-8')).hexdigest()
+        self.followed.append(Follow(followed=self))
 
     @property
     def password(self):
@@ -107,6 +108,14 @@ class User(UserMixin, db.Model):
     @password.setter
     def password(self, password):
         self.password_hash = generate_password_hash(password)
+
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
